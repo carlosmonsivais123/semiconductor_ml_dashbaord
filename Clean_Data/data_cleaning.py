9224087f0acde80a94ee3b287fe53795aaa5331d
Carlos Monsivais <carlos.s.monsivais@gmail.com>
2023-06-15 00:22:25 -0700

    added missing values visualization

--- Clean_Data/data_cleaning.py
+++ Clean_Data/data_cleaning.py
@@ -1,5 +1,13 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 class Clean_Data:
-    def data_cleaning_visualizations(self):
-        
+    def missing_values_visualizations(self, df):
+        plt.figure(figsize=(10,6))
+        sns.heatmap(df.isna(),
+                    cmap="YlGnBu",
+                    cbar_kws={'label': 'Missing Data'})
+        # plt.savefig("visualizing_missing_data_with_heatmap_Seaborn_Python.png", dpi=100)
+        plt.show()
+
