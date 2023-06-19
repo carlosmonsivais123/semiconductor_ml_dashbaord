a146e324987d0f804ab29bae0513b01947b7f0b0
Carlos Monsivais <carlos.s.monsivais@gmail.com>
2023-06-18 22:00:39 -0700

    added comments to config file read

--- Configuration_Variables/read_config.py
+++ Configuration_Variables/read_config.py
@@ -1,33 +1,37 @@
 import yaml
 
+################## Reading Config File ##################
 with open('/Users/carlosmonsivais/Desktop/secom/Configuration_Variables/config.yaml', 'r') as file:
     input_vars=yaml.safe_load(file)
 
-# Read Data Location
+
+################## Reading Data ##################
 secom_data_values_location=input_vars['Reading_Data_Location']['secom_data_values_location']
 secom_data_labels_location=input_vars['Reading_Data_Location']['secom_data_labels_location']
 
 
+################## EDA Plot Storage ##################
 # EDA Visualizatiion Graph Output
 eda_visualizatiion_graph_output=input_vars['EDA_Visualizatiion_Graph_Output']
 
 
+################## Data Cleaning Values ##################
 # Missing Values Threshold
 missing_value_threshold=input_vars['Missing_Value_Threshold']
-
 
 # Unique Value Threshold
 unique_value_threshold=input_vars['Unique_Value_Threshold']
 
-
 # Correlation Drop Threshold
 correlation_drop_threshold=input_vars['Correlation_Drop_Threshold']
 
+
+################## Random State ##################
 # Random State Value
 random_state_value=input_vars['Random_State_Value']
 
 
-
+################## Output Data ##################
 # Output Merged CSV
 merged_data_location=input_vars['Output_Data_Location']['merged_data_location']
 
@@ -39,7 +43,6 @@ clean_data_location=input_vars['Output_Data_Location']['clean_data_location']
 
 # Output Training CSV
 training_data_output_location=input_vars['Output_Data_Location']['training_data_output_location']
-
 
 # Output Testing CSV
 testing_data_output_location=input_vars['Output_Data_Location']['testing_data_output_location']
